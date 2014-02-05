1432718834ba8cddad8e2185f66e076329af7723
Dave-Choi <daaave@gmail.com>
2014-02-05 17:17:36 -0500

    Linker improvements
    - The Agricola Dictionary is sorted by priority, so that long tokens
    aren’t overwritten by shorter ones that are proper subsets of them.
    - Added a marking mechanism in the link setup so just added links
    aren’t touched, which allows the priority dictionary to work.
    - Tokens containing apostrophes now work properly.  It seems like
    Jekyll was turning them into ticks for better typography.

--- js/linker.js
+++ js/linker.js
@@ -1,22 +1,45 @@
 function setupLinks(target, dictionary){
 	// Dictionary is of the form [<token>, <href>]
-	// Super naive pattern replacement.  
+	// The dictionary should be sorted in order of priority,
+	// usually longest patterns first.
+	// e.g. If you have "Field Watchman" and "Field", you want
+	// "Field Watchman" to precede "Field" so that 
+
+	// Super naive pattern replacement.
 	// Doesn't check the token's context.  Will break some HTML.
 	var $target = $(target);
 	var string = $target.html();
 
-	for(var i=dictionary.length-1; i>=0; i--){
+	var i, len = dictionary.length;
+	for(var i=0; i<len; i++){
 		var entry = dictionary[i];
-		var tok = entry[0];
+		// Jekyll does some crap where regular apostrophes (') turn into front ticks (’) or something
+		var tok = entry[0].replace(/['’]/g, "['’]");
 		var href = entry[1];
 
-		var tokRE = new RegExp("\\b" + tok + "\\b", "g");
+		/* 
+			The [^!] part is a really rudimentary way to make sure
+			we're not writing over our own links by checking
+			the token isn't preceded by an exclamation point.
 
+			This is as close as JS gets to regex lookbehind without
+			jumping through some crazy hoops like reversing the
+			target string and all of the search patterns.
+
+			There are two captured groups:
+			$1 - The preceding non-marker character, which is just replaced before the new link.
+			$2 - The token matching the dictionary entry, which is wrapped in the link tags
+		*/
+		var tokRE = new RegExp("([^!])\\b(" + tok + ")\\b", "g");
 		string = string.replace(
 			tokRE,
-			'<a href="' + href + '">$&</a>'
+			'$1<a href="' + href + '">!$2</a>'
 		);
 	}
+
+	// Clean up the markers after we're done processing the dictionary
+	string = string.replace(/>!/g, ">");
+
 	$target.html(string);
 }
 
