1d98835e065ab41b5e85b9a7cb7b3cea68893b34
Dave Choi <daaave@gmail.com>
2015-01-06 10:24:26 -0500

    Cleanup linker.js
    Comment formatting, and redeclaration of var i.

--- js/linker.js
+++ js/linker.js
@@ -1,9 +1,13 @@
 function setupLinks(target, dictionary){
-	// Dictionary is of the form [<token>, <href>]
-	// The dictionary should be sorted in order of priority,
-	// usually longest patterns first.
-	// e.g. If you have "Field Watchman" and "Field", you want
-	// "Field Watchman" to precede "Field" so that 
+	/*
+		Dictionary is of the form [<token>, <href>]
+		The dictionary should be sorted in order of priority,
+		usually longest patterns first.
+
+		e.g. If you have "Field Watchman" and "Field", you want
+		"Field Watchman" to precede "Field" so that the full "Field Watchman"
+		phrase is linked.
+	*/
 
 	// Super naive pattern replacement.
 	// Doesn't check the token's context.  Will break some HTML.
@@ -11,13 +15,13 @@ function setupLinks(target, dictionary){
 	var string = $target.html();
 
 	var i, len = dictionary.length;
-	for(var i=0; i<len; i++){
+	for(i=0; i<len; i++){
 		var entry = dictionary[i];
 		// Jekyll does some crap where regular apostrophes (') turn into front ticks (’) or something
 		var tok = entry[0].replace(/['’]/g, "['’]");
 		var href = entry[1];
 
-		/* 
+		var tokRE = new RegExp("([^!])\\b(" + tok + ")\\b", "g"); /* 
 			The [^!] part is a really rudimentary way to make sure
 			we're not writing over our own links by checking
 			the token isn't preceded by an exclamation point.
@@ -30,7 +34,7 @@ function setupLinks(target, dictionary){
 			$1 - The preceding non-marker character, which is just replaced before the new link.
 			$2 - The token matching the dictionary entry, which is wrapped in the link tags
 		*/
-		var tokRE = new RegExp("([^!])\\b(" + tok + ")\\b", "g");
+
 		string = string.replace(
 			tokRE,
 			'$1<a href="' + href + '">!$2</a>'
